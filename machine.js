8e9818d01fef1b347cec8a5dc308bb60cc7e468f
gabrielfogaca <gabriel.fogaca@unijui.edu.br>
2022-06-05 02:25:58 -0300

    first commit

---
+++ machine.js
@@ -0,0 +1,18 @@
+function tableCreate(row, col){
+    let body = document.body;
+    let tbl  = document.createElement('table');
+    tbl.style.width  = '200px';
+    tbl.style.border = '1px solid black';
+
+    for(let i = 0; i < row; i++){
+        let tr = tbl.insertRow();
+        for(let j = 0; j < col; j++){
+                let td = tr.insertCell();
+                td.appendChild(document.createTextNode(`${i}${j}` ));
+                td.style.border = '1px solid black';
+            }     
+    }
+    body.appendChild(tbl);
+}
+
+tableCreate(16,3);
